4c9204ea0fb4219c0e7591fa4c33235e2cb9cd34
adityatripathi5 <aditya.tripathi.snaatak@mygurukulam.co>
2026-03-19 10:44:25 +0530

    adding the code

---
+++ ApiController.java
@@ -0,0 +1,18 @@
+package com.poc.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    // 🚨 HARDCODED SECRET FOR GITLEAKS & SONARQUBE ��
+    private String stripeSecretKey = "sk_live_51J0XabcDEFghiJKLmnoPQRstuVWXyz1234567890";
+
+    @GetMapping("/status")
+    public String getStatus() {
+        // 🚨 BAD PRACTICE FOR SONARQUBE 🚨
+        System.out.println("API is running with key: " + stripeSecretKey);
+        return "API is Up and Running!";
+    }
+}
